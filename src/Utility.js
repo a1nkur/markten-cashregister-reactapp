9ab85c4c3c95b3fc59013907c1e01d39d6b12120
ankur29mac <ankur29mac@gmail.com>
2021-08-18 14:39:30 +0530

    disabled eslint for looping function

--- src/Utility.js
+++ src/Utility.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-loop-func */
 export default function ExchangeCal(arr, amtToReturn, setError) {
   let finalArr;
-  let temp = amtToReturn;
+  let tempValue = amtToReturn;
 
   if (amtToReturn < 0) {
     setError("Cash is less than the bill, please enter the right amount.");
@@ -17,12 +18,12 @@ export default function ExchangeCal(arr, amtToReturn, setError) {
       }
     });
   } else {
-    while (temp !== 0) {
+    while (tempValue !== 0) {
       finalArr = arr.map(item => {
-        if (item.currenyFaceValue <= temp) {
-          let qty = Math.floor(temp / item.currenyFaceValue);
-          let remainder = temp % item.currenyFaceValue;
-          temp = remainder;
+        if (item.currenyFaceValue <= tempValue) {
+          let qty = Math.floor(tempValue / item.currenyFaceValue);
+          let remainder = tempValue % item.currenyFaceValue;
+          tempValue = remainder;
           return {
             currenyFaceValue: item.currenyFaceValue,
             quantity: qty,
